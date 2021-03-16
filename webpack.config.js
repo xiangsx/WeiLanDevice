863da803d2733b79074dc42238280f76dc081f89
v_shxsheng <v_shxsheng@tencent.com>
2021-03-16 20:13:40 +0800

    Revert "fix: async"
    This reverts commit d88e3a6b254b6f7944616988f54b77595691d0ea.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   target: 'node',
   entry: {
     lte: ['babel-polyfill', './lte/index.js'],

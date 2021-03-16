d88e3a6b254b6f7944616988f54b77595691d0ea
v_shxsheng <v_shxsheng@tencent.com>
2021-03-16 20:08:57 +0800

    fix: async

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   target: 'node',
   entry: {
     lte: './lte/index.js',

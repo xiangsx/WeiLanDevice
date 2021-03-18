ec31e6e85d5e1bcde2d8efc50530403f578ca22f
v_shxsheng <v_shxsheng@tencent.com>
2021-03-18 18:35:31 +0800

    fix: socketio 机制

--- client/index.js
+++ client/index.js
@@ -1,27 +1,42 @@
 const io = require('socket.io-client')
 
-const socket = io('http://127.0.0.1/lte', {
-    autoConnect: true,
-    withCredentials: true,
-    reconnection: true,
-});
+class WSClient {
+    constructor(host, namespace, key, options = {}) {
+        this.host = host;
+        this.namespace = namespace;
+        this.url = `ws://${host}${namespace}`;
+        this.key = key;
+        this.socket = io(this.url, {
+            autoConnect: true,
+            withCredentials: true,
+            reconnection: true,
+            ...options
+        });
+        this.initEvent();
+    }
 
-socket.on('res', (...rest) => {
-    console.log(...rest);
-});
+    initEvent() {
+        this.socket.on('connect', async () => {
+            console.log(`${this.url} connect`);
+            const res = await this.emitAsync('register', {deviceKey:this.key});
+            console.log(res);
+        });
+    }
 
-socket.on('connect', () => {
-    socket.emit('helloevent', {data:{name:'sx'}});
-});
+    emit(event, data, cb) {
+        this.socket.emit(event, data, (...rest) => {
+            console.log(`[${this.url}]:emit [${event}] data:[${JSON.stringify(data)}] res:[${JSON.stringify(...rest)}]`);
+            cb(...rest);
+        });
+    }
 
-// socket.onAny((...rest) => {
-//     console.log('onAny', ...rest);
-// });
+    emitAsync(event, data, cb) {
+        return new Promise(resolve => {
+            this.emit(event, data, resolve);
+        });
+    }
+}
 
-socket.on('error', function (err) {
-    console.log('error', err);
-});
-
-setTimeout(() => {
-    socket.emit('hello', 'hello')
-}, 1000);
+if (require.main === module) {
+    new WSClient('127.0.0.1', '/lte','asdjksahdkjsahd')
+}

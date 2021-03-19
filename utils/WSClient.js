4391ba73d724780d9593dcb84cc507ed626adb07
v_shxsheng <v_shxsheng@tencent.com>
2021-03-19 09:53:29 +0800

    feat: socketio 移为通用工具

---
+++ utils/WSClient.js
@@ -0,0 +1,44 @@
+const io = require('socket.io-client')
+
+class WSClient {
+    constructor(host, namespace, key, options = {}) {
+        this.host = host;
+        this.namespace = namespace;
+        this.url = `ws://${host}${namespace}`;
+        this.key = key;
+        this.socket = io(this.url, {
+            autoConnect: true,
+            withCredentials: true,
+            reconnection: true,
+            ...options
+        });
+        this.initEvent();
+    }
+
+    initEvent() {
+        this.socket.on('connect', async () => {
+            console.log(`${this.url} connect`);
+            const res = await this.emitAsync('register', {deviceKey:this.key});
+            console.log(res);
+        });
+    }
+
+    emit(event, data, cb) {
+        this.socket.emit(event, data, (...rest) => {
+            console.log(`[${this.url}]:emit [${event}] data:[${JSON.stringify(data)}] res:[${JSON.stringify(...rest)}]`);
+            cb(...rest);
+        });
+    }
+
+    emitAsync(event, data) {
+        return new Promise(resolve => {
+            this.emit(event, data, resolve);
+        });
+    }
+}
+
+module.exports = WSClient;
+
+if (require.main === module) {
+    new WSClient('127.0.0.1', '/lte','asdjksahdkjsahd')
+}

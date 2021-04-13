7411c09420b3b00d9d8a63eabb35e7bc7bc871d2
v_shxsheng <v_shxsheng@tencent.com>
2021-04-13 14:18:46 +0800

    feat: socketIO 压缩发送

--- utils/WSClient.js
+++ utils/WSClient.js
@@ -10,7 +10,7 @@ class WSClient {
             withCredentials: true,
             reconnection: true,
             ...options
-        });
+        }).compress(true);
         this.initLog();
         this.init();
     }

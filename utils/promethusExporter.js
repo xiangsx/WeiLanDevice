b0f62f886a672652e0c3e5facb4a07b15b252bab
xiang <1984871009@qq.com>
2021-07-17 13:25:13 +0800

    fix：定时发送监控metrics

--- utils/promethusExporter.js
+++ utils/promethusExporter.js
@@ -1,10 +1,11 @@
 import client from 'prom-client'
-import Config from '../config/config'
 import http from 'http';
+import {scheduleJob} from 'node-schedule'
+import Config from '../config/config'
 import {generateDeviceID} from "./tools";
 import {EnumDeviceType} from "../define/device";
 
-const gateway = new client.Pushgateway(`http://${Config.wsServerCfg.host}:${Config.publishGatewayPort}`, {
+const gateway = new client.Pushgateway(`http://${Config.wsServerCfg.host}:${Config.pushGatewayPort}`, {
     timeout: 5000, //Set the request timeout to 5000ms
     agent: new http.Agent({
         keepAlive: true,
@@ -13,19 +14,28 @@ const gateway = new client.Pushgateway(`http://${Config.wsServerCfg.host}:${Conf
     }),
 });
 
-gateway.pushAdd({jobName: 'device-exporter'}, function (err, resp, body) {
-    if (err) {
-        console.error(err);
-    }
-    else {
-        console.log(`promethus init successfully!!!`);
-    }
-}); //Add metric and overwrite old ones
+function push() {
+    gateway.push({
+        jobName: 'device-exporter',
+        groupings: {
+            instance: `${deviceID}|${process.env.PN}`
+        },
+    }, function (err, resp, body) {
+        if (err) {
+            console.error(err);
+        } else {
+            console.debug(`promethus init successfully!!!`);
+        }
+    })
+}
+
 const deviceID = generateDeviceID(EnumDeviceType.LTE);
 
 client.collectDefaultMetrics({
-    prefix: generateDeviceID(EnumDeviceType.LTE), labels: {
+    labels: {
         process: process.env.PN,
         deviceID,
     }
 });
+
+scheduleJob(`*/${Config.pushInterval} * * * * *`, push);
